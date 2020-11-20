93ec4eeed549a6c1bfd2ebe52ae34123d96ab90c
ZhaoYibo <zhaoyibochn@gmail.com>
2020-11-20 16:18:57 +0800

    时间窗

--- time-window/src/main/java/com/haoyizebo/window/model/FixedWindow.java
+++ time-window/src/main/java/com/haoyizebo/window/model/FixedWindow.java
@@ -2,6 +2,7 @@ package com.haoyizebo.window.model;
 
 
 import com.haoyizebo.window.constant.WindowTypeEnum;
+import org.apache.commons.lang3.math.NumberUtils;
 import redis.clients.jedis.Jedis;
 
 /**
@@ -19,25 +20,16 @@ class FixedWindow extends AbstractWindow implements IWindow {
             return true;
         }
 
-
-        Jedis redis = getJedis();
+        Jedis jedis = getJedis();
 
         String redisKey = timeWindow.getRedisKey(getWindowType(), uid);
 
-        long val = redis.incrBy(redisKey, amount);
+        long val = jedis.incrBy(redisKey, amount);
         if (val == amount) {
-            redis.expire(redisKey, seconds);
+            jedis.expire(redisKey, seconds);
         }
         if (val > threshold) {
-            // 兜底策略，避免上边 expire 没有设置上。
-            // ttl 返回值说明：
-            //      当 key 不存在时，返回 -2 。
-            //      当 key 存在但没有设置剩余生存时间时，返回 -1 。
-            //      否则，以秒为单位，返回 key 的剩余生存时间。
-            long ttl = redis.ttl(redisKey);
-            if (ttl == -1) {
-                redis.del(redisKey);
-            }
+            guarantee(getJedis(), redisKey);
             return false;
         }
         return true;
@@ -45,7 +37,40 @@ class FixedWindow extends AbstractWindow implements IWindow {
 
     @Override
     public boolean compareThenIncr(TimeWindow timeWindow, String uid, long amount, long threshold) {
+        int seconds = Math.toIntExact(timeWindow.getParentSeconds());
+        if (seconds <= 0) {
+            return true;
+        }
+
+        Jedis jedis = getJedis();
+        String redisKey = timeWindow.getRedisKey(getWindowType(), uid);
+
+        long val = amount;
+
+        String s = jedis.get(redisKey);
+        if (NumberUtils.isDigits(s)) {
+            val += Long.parseLong(s);
+        }
+
+        if (val > threshold) {
+            guarantee(jedis, redisKey);
+            return false;
+        }
+
+        jedis.incrBy(redisKey, amount);
         return false;
+    }
+
+    private void guarantee(Jedis jedis, String redisKey) {
+        // 兜底策略，避免上边 expire 没有设置上。
+        // ttl 返回值说明：
+        //      当 key 不存在时，返回 -2 。
+        //      当 key 存在但没有设置剩余生存时间时，返回 -1 。
+        //      否则，以秒为单位，返回 key 的剩余生存时间。
+        long ttl = jedis.ttl(redisKey);
+        if (ttl == -1) {
+            jedis.del(redisKey);
+        }
     }
 
     @Override
--- time-window/src/main/java/com/haoyizebo/window/model/SlidingWindow.java
+++ time-window/src/main/java/com/haoyizebo/window/model/SlidingWindow.java
@@ -23,10 +23,60 @@ class SlidingWindow extends AbstractWindow implements IWindow {
         String redisKey = timeWindow.getRedisKey(getWindowType(), uid);
         String field = timeWindow.generateChildKey();
 
+        doIncr(tairHash, redisKey, field, amount, seconds);
+
+        long sum = getBeforeAmount(tairHash, redisKey);
+
+        if (sum > threshold) {
+            guarantee(redisKey, timeWindow);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public boolean compareThenIncr(TimeWindow timeWindow, String uid, long amount, long threshold) {
+        int seconds = Math.toIntExact(timeWindow.getParentSeconds());
+        if (seconds <= 0) {
+            return true;
+        }
+
+        TairHash tairHash = getTairHash();
+
+        String redisKey = timeWindow.getRedisKey(getWindowType(), uid);
+
+        long sum = amount + getBeforeAmount(tairHash, redisKey);
+
+        if (sum > threshold) {
+            guarantee(redisKey, timeWindow);
+            return false;
+        }
+
+        String field = timeWindow.generateChildKey();
+
+        doIncr(tairHash, redisKey, field, amount, seconds);
+
+        return true;
+    }
+
+    @Override
+    public WindowTypeEnum getWindowType() {
+        return WindowTypeEnum.SLIDING;
+    }
+
+    private long getBeforeAmount(TairHash tairHash, String redisKey) {
+        return tairHash.exhvals(redisKey)
+                .stream().mapToLong(Long::parseLong).sum();
+    }
+
+    private void doIncr(TairHash tairHash, String redisKey, String field, long amount, int seconds) {
+        // 当 amount == 0 时，incr 无意义
         if (amount != 0) {
             Long val = tairHash.exhincrBy(redisKey, field, amount);
             if (val == amount) {
-                // 避免 -1 + 1 + 1 这样类似 ABA 问题。
+                // 理论上 val == amount 只出现在第一次时候，
+                // 但为了避免 -1 + 1 + 1 这样类似 ABA 问题，再 ttl 一次
                 // exhttl 返回值说明：
                 //      key或者field不存在：-2。
                 //      field存在但是没有设置过期时间：-1。
@@ -37,27 +87,11 @@ class SlidingWindow extends AbstractWindow implements IWindow {
                 }
             }
         }
-
-        long sum = tairHash.exhvals(redisKey)
-                .stream().mapToLong(Long::parseLong).sum();
-
-        if (sum > threshold) {
-            // 兜底，给 redis key 设一个 x2 过期时间
-            getJedis().expire(redisKey, (int) (timeWindow.getParentSeconds() * 2));
-            return false;
-        } else {
-            return true;
-        }
     }
 
-    @Override
-    public boolean compareThenIncr(TimeWindow timeWindow, String uid, long amount, long threshold) {
-        return false;
-    }
-
-    @Override
-    public WindowTypeEnum getWindowType() {
-        return WindowTypeEnum.SLIDING;
+    private void guarantee(String redisKey, TimeWindow timeWindow) {
+        // 兜底策略，给 redis key 设一个 x2 过期时间
+        getJedis().expire(redisKey, (int) (timeWindow.getParentSeconds() * 2));
     }
 
 }
